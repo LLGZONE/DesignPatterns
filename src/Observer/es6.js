35ff6173d5073781a708d911052953d611645244
llgzone <985395976@qq.com>
2018-03-08 14:24:59 +0800

    add pubsub

---
+++ src/Observer/es6.js
@@ -0,0 +1,54 @@
+let cObserver = null
+
+{
+  const handlers = {}
+  let uid = 0
+
+  class Observer {
+    addListener(type, fn) {
+      if (!handlers[type]) {
+        handlers[type] = []
+      }
+
+      const id = uid++
+      handlers[type].push({id, func: fn})
+
+      return id
+    }
+
+    fire(type, ...args) {
+      if (handlers[type] && handlers[type] instanceof Array) {
+        let len = handlers[type].length
+
+        while (len--) {
+          handlers[type][len].func(...args)
+        }
+      }
+    }
+
+    removeListener(id) {
+      for (let key in handlers) {
+        if (handlers[key]) {
+          for (let i = 0; i < handlers[key].length; i++) {
+            if (handlers[key][i].id === id) {
+              handlers[key].splice(i, 1)
+              break
+            }
+          }
+        }
+      }
+    }
+  }
+
+  cObserver = new Observer()
+}
+
+const id = cObserver.addListener('message', (...args) => {
+  console.log(args.join(','))
+})
+
+cObserver.fire('message', 'hello', 'world')
+
+cObserver.removeListener(id)
+
+cObserver.fire('message', 'hello', 'world')
